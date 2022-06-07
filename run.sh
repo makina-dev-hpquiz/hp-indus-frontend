4f043834ce815723e84833432c1b95bd132c96e4
Makina Dev <makina.informatique@gmail.com>
2022-06-07 17:42:48 +0400

    Démarrage rapide : Mise en place de script de démarrage/extinction rapide de l'environnement en mode dev

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-ionic serve --external
+# ionic serve --external
+echo "[DEV MODE] HP-INDUS-FRONTEND démarré, il sera dispoible dans quelques instants."
+nohup ionic serve --external & echo $! > ./pid.file &
---
+++ stop.sh
@@ -0,0 +1,10 @@
+
+echo "[DEV MODE] EXTINCTION DE HP-INDUS-FRONTEND - PID : " $(cat ./pid.file)
+kill $(cat ./pid.file)
+echo ;
+
+wait -n $(cat ./pid.file);
+
+#Suppression fichier
+rm pid.file
+rm nohup.out
