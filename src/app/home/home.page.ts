2de64414ffb72de07544215f44a19a652c751797
Makina Dev <makina.informatique@gmail.com>
2022-06-07 20:09:51 +0400

    Gestion APK : Harmoniser le mode de récupération des différents APK

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import {AndroidPackage} from 'src/entities/androidPackage';
 
 @Component({
   selector: 'app-home',
@@ -7,25 +8,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-  // 192.168.1.11
-  private apiUrl = "http://192.168.1.11:8082/apks/last"; 
+  // 192.168.1.11 192.168.43.20
+  private apiUrl = "http://192.168.43.20:8082/apks/";
   public lastAPKPath: string;
+
+  public hpCoreAPK: AndroidPackage;
+  public hpQuizAPK: AndroidPackage;
 
   constructor(private httpClient: HttpClient) {
   }
 
   ngOnInit() {
 
-     this.getLastAPK();
+    this.getLastHPQuizAPK();
+    this.getLastHPCoreApk();
   }
 
-  async getLastAPK(){
-    this.httpClient.get<any>(this.apiUrl).subscribe(data => {
-     this.lastAPKPath = data.response;
-  });
+  /**
+   * Interroge le serveur pour récupérer l'adresse et des informations sur la dernière APK HP-Quiz
+   */
+  async getLastHPQuizAPK() {
+    let  url = "lastHPQuizAPK";
+    this.httpClient.get<any>(this.apiUrl+url).subscribe(data => {
+      this.hpQuizAPK = data;
+      console.log(this.hpQuizAPK);
+    });
 
-
- 
     // this.lastAPKPath = "../../assets/apk/app-debug.apk";
   }
+
+  /**
+  * Interroge le serveur pour récupérer l'adresse et des informations sur la dernière APK HP-Quiz
+  */
+  async getLastHPCoreApk() {
+    let url = "lastHPCoreAPK";
+    this.httpClient.get<any>(this.apiUrl+url).subscribe(data => {
+      this.hpCoreAPK = data;
+    });
+  }
+
+
 }
---
+++ src/entities/androidPackage.ts
@@ -0,0 +1,8 @@
+export class AndroidPackage {
+    constructor(public name: string,
+        public buildDate: string,
+        public size: string,
+        public version: string,
+        public path?: string,){}
+
+}
