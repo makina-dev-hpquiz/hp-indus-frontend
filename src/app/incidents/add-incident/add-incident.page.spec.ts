9cf79ee38a0371d5ea23a8a7d363bd1c672c9139
Makina Dev <makina.informatique@gmail.com>
2022-08-19 20:38:39 +0400

    AddIncidentPage Ajout de test d'initialisation de la page

--- src/app/incidents/add-incident/add-incident.page.spec.ts
+++ src/app/incidents/add-incident/add-incident.page.spec.ts
@@ -22,31 +22,31 @@ describe('AddIncidentPage', () => {
   let mockImageInputComponent: jasmine.SpyObj<ImageInputComponent>;
 
   // Nom de méthodes privées
-  let addIncident = 'addIncident';
-  let updateIncident = 'updateIncident';
-  let formValueIsComplete = 'formValueIsComplete';
-  let generateIncidentFormData = 'generateIncidentFormData';
-  let getIncident = 'getIncident';
-  let deleteIncident = 'deleteIncident';
-  // Nom de propriétés privées 
-  let router = 'router';
-  let incidentPage = 'INCIDENTS_PAGE';
+  const addIncident = 'addIncident';
+  const updateIncident = 'updateIncident';
+  const formValueIsComplete = 'formValueIsComplete';
+  const generateIncidentFormData = 'generateIncidentFormData';
+  const getIncident = 'getIncident';
+  const deleteIncident = 'deleteIncident';
+  // Nom de propriétés privées
+  const router = 'router';
+  const incidentPage = 'INCIDENTS_PAGE';
 
   // DATA TESTING
   const currentDate = new Date();
 
   const formValue = {
-    'id': 'f0de50b4-a33a-4cde-8587-876a9e8851ab',
-    'title': 'Test 1',
-    'description': 'Description du test 1',
-    'screenshotPath': '',
-    'screenshotWebPath': '',
-    'priority': 'normal',
-    'createdAt': currentDate.toISOString(),
-    'updatedAt': currentDate.toISOString(),
-    'type': 'interface',
-    'status': 'en cours'
-  }
+    id: 'f0de50b4-a33a-4cde-8587-876a9e8851ab',
+    title: 'Test 1',
+    description: 'Description du test 1',
+    screenshotPath: '',
+    screenshotWebPath: '',
+    priority: 'normal',
+    createdAt: currentDate.toISOString(),
+    updatedAt: currentDate.toISOString(),
+    type: 'interface',
+    status: 'en cours'
+  };
 
   const incident = new Incident('f0de50b4-a33a-4cde-8587-876a9e8851ab',
     'Test 1',
@@ -87,20 +87,65 @@ describe('AddIncidentPage', () => {
     }).compileComponents();
     fixture = TestBed.createComponent(AddIncidentPage);
     component = fixture.componentInstance;
-    
+
     fixture.detectChanges();
   }));
 
-  it('should create', async () => {
+  it('Est initialisé avec l\'état NEW', async () => {
     const incidentProperty = new IncidentProperty(['type 1', 'type 2'], ['type 1', 'type 2', 'type 3'], 'type 1');
 
+    await mockIncidentPropertiesService.getPriorities.and.returnValue(of(incidentProperty).toPromise());
+    await mockIncidentPropertiesService.getStatus.and.returnValue(of(incidentProperty).toPromise());
+    await mockIncidentPropertiesService.getTypes.and.returnValue(of(incidentProperty).toPromise());
+
+    await component.ngOnInit();
+    expect(component).toBeTruthy();
+
+    expect(incidentProperty.properties).toEqual(component.prioritiesList);
+    expect(incidentProperty.properties).toEqual(component.statusList);
+    expect(incidentProperty.properties).toEqual(component.typesList);
+
+    expect(incidentProperty.defaultProperty).toEqual(component.incident.status);
+    expect(incidentProperty.defaultProperty).toEqual(component.incident.priority);
+
+    expect(component.STATE_NEW).toEqual(component.state);
+
+    // La date de création doit être la même que la date de mise à jour
+    expect(component.incident.createdAt.toISOString()).toEqual(component.createdAt);
+    expect(component.createdAt).toEqual(component.updatedAt);
+
+    // Les valeurs screenshotPath / screenshotWebPath / description sont initialisées à ""
+    expect('').toEqual(component.incident.screenshotPath);
+    expect('').toEqual(component.incident.screenshotWebPath);
+    expect('').toEqual(component.incident.description);
+  });
+
+  it('Est initialisé avec l\'état UPDATE', async () => {
+    const incidentProperty = new IncidentProperty(['type 1', 'type 2'], ['type 1', 'type 2', 'type 3'], 'type 1');
+
+    incident.screenshotWebPath = 'localhost:8080/images/img.jpg';
+    component.incidentService = mockIncidentService;
     await mockIncidentService.get.and.returnValue(of(incident).toPromise());
     await mockIncidentPropertiesService.getPriorities.and.returnValue(of(incidentProperty).toPromise());
     await mockIncidentPropertiesService.getStatus.and.returnValue(of(incidentProperty).toPromise());
     await mockIncidentPropertiesService.getTypes.and.returnValue(of(incidentProperty).toPromise());
 
+    const id = 'f0de50b4-a33a-4cde-8587-876a9e8851ab';
+    component.route.snapshot.data.special = id;
+
+    await component.ngOnInit();
     expect(component).toBeTruthy();
-    //TODO Rajouter de expect  
+
+    expect(incidentProperty.properties).toEqual(component.prioritiesList);
+    expect(incidentProperty.properties).toEqual(component.statusList);
+    expect(incidentProperty.properties).toEqual(component.typesList);
+
+    expect(component.STATE_UPDATE).toEqual(component.state);
+    expect(id).toEqual(component.incident.id);
+
+    expect(component.incident.screenshotWebPath).toEqual(component.screenshot);
+    expect(component.incident.createdAt.toISOString()).toEqual(component.createdAt);
+    expect(component.incident.updatedAt.toISOString()).toEqual(component.updatedAt);
   });
 
   it('TEST public AddIncidentPage.saveAction', async () => {
@@ -108,9 +153,9 @@ describe('AddIncidentPage', () => {
     mockImageInputComponent.getFile.and.returnValue(of(''));
 
     component.state = component.STATE_NEW;
-    
-    let addIncidentSpy = spyOn<any>(component, addIncident);
-    let updateIncidentSpy = spyOn<any>(component, updateIncident);
+
+    const addIncidentSpy = spyOn<any>(component, addIncident);
+    const updateIncidentSpy = spyOn<any>(component, updateIncident);
 
     // TEST Création Incident avec un objet Incident INcomplet
     await component.saveAction(formValue);
@@ -123,7 +168,6 @@ describe('AddIncidentPage', () => {
     expect(addIncidentSpy).toHaveBeenCalledTimes(1);
     expect(updateIncidentSpy).toHaveBeenCalledTimes(0);
 
-    
     // TEST Mise à jour Incident avec un objet Incident Incomplet
     component.state = component.STATE_UPDATE;
     await component.saveAction(formValue);
@@ -147,7 +191,7 @@ describe('AddIncidentPage', () => {
   });
 
   it('TEST public AddIncidentPage.deleteIncident', async () => {
-    spyOn(window, 'confirm').and.callFake(function () {
+    spyOn(window, 'confirm').and.callFake(function() {
       return true;
     });
     let t: any;
@@ -156,7 +200,7 @@ describe('AddIncidentPage', () => {
     expect(component[incidentPage]).toEqual(component[router].url);
   });
 
-  it('TEST public AddIncidentPage.cancelImage', () => {
+  it('TEST public AddIncidentPage.cancelImage retire les images de l\'objet incident', () => {
 
     const screenshotPath = 'src/content/img1.jpg';
     const screenshotWebPath = 'localhost:8080/images/img1.jpg';
@@ -173,8 +217,8 @@ describe('AddIncidentPage', () => {
   });
 
   it('TEST private AddIncidentPage.formValueIsComplete', () => {
-    const incident = new Incident('f0de50b4-a33a-4cde-8587-876a9e8851ab');
-    component.incident = incident;
+    const incidentUseOnFormValue = new Incident('f0de50b4-a33a-4cde-8587-876a9e8851ab');
+    component.incident = incidentUseOnFormValue;
     expect(component[formValueIsComplete]()).toBeFalse();
 
     component.incident.title = 'TITRE';
@@ -196,7 +240,7 @@ describe('AddIncidentPage', () => {
 
   });
 
-  it('TEST private AddIncidentPage.addIncident', async () => {
+  it('TEST private AddIncidentPage.addIncident sauvegarde un incident et retourne à la page liste des incidents', async () => {
     mockIncidentService.save.and.returnValue(of(incident).toPromise());
     component.imageInput = mockImageInputComponent;
     mockImageInputComponent.getFile.and.returnValue(of(''));
@@ -205,7 +249,7 @@ describe('AddIncidentPage', () => {
     expect(component[incidentPage]).toEqual(component[router].url);
   });
 
-  it('TEST private AddIncidentPage.updateIncident', async () => {
+  it('TEST private AddIncidentPage.updateIncident met à jour un incident et retourne à la page liste des incidents', async () => {
     mockIncidentService.update.and.returnValue(of(incident).toPromise());
     component.imageInput = mockImageInputComponent;
     mockImageInputComponent.getFile.and.returnValue(of(''));
@@ -214,9 +258,9 @@ describe('AddIncidentPage', () => {
     expect(component[incidentPage]).toEqual(component[router].url);
   });
 
-  it('TEST private AddIncidentPage.getIncident()', async () => {
+  it('TEST private AddIncidentPage.getIncident() récupère un incident', async () => {
     mockIncidentService.get.and.returnValue(of(incident).toPromise());
-    let result = await component[getIncident]('f0de50b4-a33a-4cde-8587-876a9e8851ab');
+    const result = await component[getIncident]('f0de50b4-a33a-4cde-8587-876a9e8851ab');
     expect(result).toEqual(incident);
   });
 
@@ -224,19 +268,19 @@ describe('AddIncidentPage', () => {
     // TODO A Impl.
   });
 
-  it('TEST private generateIncidentFormData', () => {
+  it('TEST private AddIncidentPage.generateIncidentFormData ', () => {
     component.imageInput = mockImageInputComponent;
     mockImageInputComponent.getFile.and.returnValue(of(''));
 
-    let result = component[generateIncidentFormData](formValue);
+    const result = component[generateIncidentFormData](formValue);
 
-    let resutltFile: string = (result.get('file') as File).name
-    let resutltTitle: string = result.get('title') as string;
-    
+    const resutltFile: string = (result.get('file') as File).name;
+    const resutltTitle: string = result.get('title') as string;
+
     expect(undefined).toEqual(resutltFile);
     expect('Test 1').toEqual(resutltTitle);
 
-    //TODO Impl. des tests plus approfondies lorsqu'il y a un fichier et lorsqu'il y en a pas 
+    //TODO Impl. des tests plus approfondies lorsqu'il y a un fichier et lorsqu'il y en a pas
   });
 
 
